b046722dac342d873d43749a661b9a37e584bc90
Yoko Harada <yokolet@gmail.com>
2013-08-31 11:56:29 -0400

    initial commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShamrockBlend::Application.config.secret_token = '54999fab8b66beed1edc31ce82a628d984f074cf4a51d15c4377294c57b05551ba9f69a4e09d305ce0e64bc756a86e9e6b875c05811004c85540d9bb476db06d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShamrockBlend::Application.config.session_store :cookie_store, key: '_shamrock.blend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShamrockBlend::Application.config.session_store :active_record_store
