a300c7994718e656472538ef6bdbf522cfaedc9e
Yoko Harada <yokolet@gmail.com>
2013-08-31 13:00:35 -0400

    Adds diametric config and resolve conflict with devise

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+require 'devise/orm/active_record'
+
 module ShamrockBlend
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
