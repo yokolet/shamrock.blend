697c6a9771814e1d6ffc7726c000dbfe681f9608
Yoko Harada <yokolet@gmail.com>
2013-09-02 22:26:10 -0400

    Update for 16 pages

--- app/controllers/slideshow_controller.rb
+++ app/controllers/slideshow_controller.rb
@@ -1,6 +1,8 @@
 class SlideshowController < ApplicationController
   def index
-    @page = Page.get(Page.where(order: 0).first)
+    page = Page.get(Page.where(order: 0).first)
+    @title = page.title
+    @content = Haml::Engine.new(page.content).render
     @number_of_pages = Page.all.length
     respond_to do |format|
       format.html
@@ -10,8 +12,12 @@ class SlideshowController < ApplicationController
 
   def show
     @no = params[:id].to_i
-    @page = Page.get(Page.where(order: @no).first)
+    page = Page.get(Page.where(order: @no).first)
     @number_of_pages = Page.all.length
+    @title = page.title
+    @content = Haml::Engine.new(page.content).render
+    puts "#{page.title}, #{page.content}"
+    puts @content
     respond_to do |format|
       format.html
       format.js
